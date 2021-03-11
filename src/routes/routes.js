61cc1d5cafd1a035651b8bd17eced6ba114b6e34
Denicoli <tiagodenicoli@hotmail.com>
2021-03-11 11:20:25 -0300

    Route changes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const dataController = require('../controllers/data.crontroller');
 
 router.get('/', dataController.list);
-router.post('/', dataController.create);
-
 router.post('/populate', dataController.populate);
 
 module.exports = router;
